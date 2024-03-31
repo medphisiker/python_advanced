5037e67f6a3f8039935e55a62f226d722a7436a2
Anton Shiryaev <med.phisiker@gmail.com>
2024-04-01 02:13:56 +1000

    feat(3.1 solution): modify to solve 3.1 task
    modify to solve 3.1 task

--- hw3/3.1_example.py
+++ hw3/3.1_example.py
@@ -3,14 +3,14 @@ import numpy as np
 
 if __name__ == "__main__":
     # Set the seed
-    np.random.seed(42)
+    np.random.seed(0)
 
     # Создаем две матрицы
-    matrix1_np = np.random.randint(0, 10, (2, 2))
+    matrix1_np = np.random.randint(0, 10, (10, 10))
     matrix1 = matrix1_np.tolist()
     matrix1 = Matrix(matrix1)
 
-    matrix2_np = np.random.randint(0, 10, (2, 2))
+    matrix2_np = np.random.randint(0, 10, (10, 10)) 
     matrix2 = matrix2_np.tolist()
     matrix2 = Matrix(matrix2)
 
@@ -21,18 +21,27 @@ if __name__ == "__main__":
 
     print("\nПоэлементное сложение матриц:")
     print("Matrix module")
-    print(matrix1 + matrix2)
+    element_wise_sum = matrix1 + matrix2
+    print(element_wise_sum)
+    element_wise_sum.write_to_file('hw3/artifacts/3.1/matrix+.txt')
+    
     print("Numpy")
     print(matrix1_np + matrix2_np)
 
     print("\nПоэлементное умножение матриц:")
     print("Matrix module")
-    print(matrix1 * matrix2)
+    element_wise_prod = matrix1 * matrix2
+    print(element_wise_prod)
+    element_wise_prod.write_to_file('hw3/artifacts/3.1/matrix*.txt')
+    
     print("Numpy")
     print(matrix1_np * matrix2_np)
 
     print("\nУмножение матриц:")
     print("Matrix module")
-    print(matrix1 @ matrix2)
+    matrix_prod = matrix1 @ matrix2
+    print(matrix_prod)
+    matrix_prod.write_to_file('hw3/artifacts/3.1/matrix@.txt')
+    
     print("Numpy")
     print(matrix1_np @ matrix2_np)
